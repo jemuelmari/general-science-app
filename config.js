f6638ba68b209f3ec4d25c5b560412deee36e5ea
jemuelmari <jemuel.mari@deped.gov.ph>
2026-09-19 18:49:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   },
 
   // ---------- Backend (Google Apps Script) ----------
-  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbwagiA7udhYQJGcN2T3FO6biHr2GhSZ-3j45Gvs5BoFxF6AARoadCWP3T8p_hPEmLuc/exec',
+  BACKEND_URL: 'https://script.google.com/macros/s/AKfycbzjd_jtwESdNPpP9Phv-ElmJcoywng3HY0r-BRVD3Ms7bDiH7G9xNGOrnFV3LQQAXSX/exec',
 
   get backendEnabled() {
     return this.BACKEND_URL && this.BACKEND_URL.length > 20;
